af02884448e71c4dbd44747a1139e357e42d83d1
Tj <tejbirwason@gmail.com>
2024-08-01 23:58:49 -0600

    More cleanup

--- frontend/src/components/JobListingShell.tsx
+++ frontend/src/components/JobListingShell.tsx
@@ -30,8 +30,6 @@ const JobListingShell: React.FC = () => {
     (a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
   );
 
-  console.log('jobs', jobs);
-
   const handleJobSelect = (job: Job) => {
     setSelectedJob(job);
   };
